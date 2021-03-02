8e6f9928513e80357d0716f583c23f1a09bd6016
shitouer0515 <shitouer0515@outlook.com>
2021-03-02 19:07:31 +0800

    Add files via upload

---
+++ export_sharecodes.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+## 路径、环境判断
+ShellDir=${JD_DIR:-$(cd $(dirname $0); pwd)}
+LogDir=${ShellDir}/log
+[[ ${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT} ]] && Opt="P" || Opt="E"
+Tips="从日志中未找到任何互助码..."
+
+## 所有有互助码的活动，只需要把脚本名称去掉前缀jd_后列在Name1中，将其中文名称列在Name2中即可。Name1和Name2中两个名称必须一一对应！
+Name1=(fruit pet plantBean dreamFactory jdfactory crazy_joy jxnc bookshop cash sgmh cfd)
+Name2=(东东农场 东东萌宠 京东种豆得豆 京喜工厂 东东工厂 crazyJoy任务 京喜农场 口袋书店 签到领现金 闪购盲盒 京喜财富岛)
+
+## 导出互助码的通用程序.
+function Cat_Scodes {
+  if [ $1 != "cfd" ] && [ -d ${LogDir}/jd_$1 ] && [[ $(ls ${LogDir}/jd_$1) != "" ]]; then
+    cd ${LogDir}/jd_$1
+    for log in $(ls -r); do
+      case $# in
+        1)
+          codes=$(cat ${log} | grep -${Opt} "开始【京东账号|您的(好友)?助力码为" | uniq | perl -0777 -pe "{s|\*||g; s|开始||g; s|\n您的(好友)?助力码为(：)?:?|：|g; s|，.+||g}" | perl -ne '{print if /：/}')
+          ;;
+        2)
+          codes=$(grep -${Opt} $2 ${log} | perl -pe "{s| ||g; s|$2||g}")
+          ;;
+      esac
+      [[ ${codes} ]] && break
+    done
+    [[ ${codes} ]] && echo "${codes}" || echo ${Tips}
+  elif [ $1 = "cfd" ] && [ -d ${LogDir}/jd_cfd ] && [[ $(ls ${LogDir}/jd_cfd) != "" ]]; then
+    cd ${LogDir}/jd_$1
+    for log in $(ls -r); do
+      case $# in
+        1)
+          codes=$(cat ${log} | grep -${Opt} "开始【京东账号|【🏖岛主】你的互助码" | uniq | perl -0777 -pe "{s|\*||g; s|开始||g; s|\n【🏖岛主】你的互助码(：)?:?|：|g; s|，.+||g}" | perl -ne '{print if /：/}')
+          ;;
+        2)
+          codes=$(grep -${Opt} $2 ${log} | perl -pe "{s| ||g; s|$2||g}")
+          ;;
+      esac
+      [[ ${codes} ]] && break
+    done
+    [[ ${codes} ]] && echo "${codes}" | sed s/[[:space:]]//g || echo ${Tips}
+  else
+    echo "还没有运行过 jd_$1 脚本，没有产生日志..."
+  fi
+}
+## 汇总
+function Cat_All {
+  echo -e "\n本脚本从最后一个正常的日志中寻找互助码，某些账号缺失则代表在最后一个正常的日志中没有找到。"
+  for ((i=0; i<${#Name1[*]}; i++)); do
+    echo -e "\n${Name2[i]}："
+    [[ $(Cat_Scodes "${Name1[i]}" "的${Name2[i]}好友互助码") == ${Tips} ]] && Cat_Scodes "${Name1[i]}" || Cat_Scodes "${Name1[i]}" "的${Name2[i]}好友互助码"
+  done
+}
+
+## 执行并写入日志
+LogTime=$(date "+%Y-%m-%d-%H-%M-%S")
+LogFile="${LogDir}/export_sharecodes/${LogTime}.log"
+[ ! -d "${LogDir}/export_sharecodes" ] && mkdir -p ${LogDir}/export_sharecodes
+Cat_All | perl -pe "{s|京东种豆|种豆|; s|crazyJoy任务|疯狂的JOY|}" | tee ${LogFile}
